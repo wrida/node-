80ad6b61cd134cea7880654472c2e4c190c4438b
warda <ghanmi.warda81@gmail.com>
2021-12-07 17:27:33 +0100

    first commit

--- Routes/userRoute.js
+++ Routes/userRoute.js
@@ -36,9 +36,9 @@ router.post("/createuser", (req, res) => {
   // find and update
   router.put("/updateuser/:name", (req, res) => {
     User.findOneAndUpdate({ name: req.params.name},
-      {$set :{age: req.body.age}},{new:true}
+      {$set :{age: req.body.age}},{new:true})
       .then((data) => res.send(data))
-      .catch((err) => res.send(err)));
+      .catch((err) => res.send(err));
    
   })
     
@@ -51,14 +51,21 @@ router.delete("/deleteuser/:id", (req, res) => {
   //remove a user
   router.remove("/removeuser/:name", (req, res) => {
     User.remove({name:"Mary"},
-    function(isDone,
-      deletedUser) {
-      return{
-        res === isDone ? 
-        "Delete success!": "Delete failed!",
-        affectedUsers: deletedUser
-      }
+    function done(data) {
+      res.json(data)
     }
     )
   })
+  // chain search 
+    router.get('/searchuser/:name', (req, res) => {
+    function done(data) {
+    const foodToSearch = "burrito";
+    Person.find({favoriteFoods:foodToSearch}).sort({ name: 1 }).limit(2).select('-age').exec((err,data) =>{   
+      
+      err ? done(err): done(null, data);
+      
+    })
+    
+  }
+})
 module.exports = router
